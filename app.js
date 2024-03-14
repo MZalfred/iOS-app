96fb6e3c8c63edd9aa0a06dc1ec5c6f03a600437
MZalfred <122396335+MZalfred@users.noreply.github.com>
2024-03-14 19:02:01 +0000

    main

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Alfred!'));
 
 app.listen(port,  () => console.log(`ACE running on port ${port}!`));
